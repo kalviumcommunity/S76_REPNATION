77486f708c041815c340f82892e5aeb325288b2f
pawin46 <pawinde@gmail.com>
2025-03-17 10:23:02 +0530

    completed Setting up React app

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100 text-2xl font-bold text-blue-600">
+      Welcome to RepNation - Your Ultimate Exercise Guide!
+    </div>
+  );
+};
+
+export default App;
